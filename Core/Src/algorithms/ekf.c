dd95ad663f822ec5d3dcca8c92c82fa08aae5f82
Alex <climie.alex@gmail.com>
2025-11-13 10:37:02 -0800

    EKF State Transition
    piece by piece.....

--- Core/Src/algorithms/ekf.c
+++ Core/Src/algorithms/ekf.c
@@ -1,40 +1,13 @@
 #include "ekf.h"
 #include <math.h>
 
-static EKF ekf;
+/* ------------- helpers ---------------- */
 
-void init_ekf(
-    float process_noise[STATE_DIM][STATE_DIM],
-    float measurement_noise[3][3])
-{
-    // init
-    ekf.x[0] = 1;
-    ekf.x[1] = 0;
-    ekf.x[2] = 0;
-    ekf.x[3] = 0;
-
-    // sets ekf.P to an identity matrix
-    for (int i = 0; i < STATE_DIM; i++) for (int j = 0; j < STATE_DIM; j++) {
-        if (i==j) {
-            ekf.P[i][j] = 1;
-            continue;
-        }
-
-        ekf.P[i][j] = 0;
-    }
-
-    // copy inputs
-    for (int i = 0; i < STATE_DIM; i++) for (int j = 0; j < STATE_DIM; j++) ekf.Q[i][j] = process_noise[i][j];
-    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) ekf.R[i][j] = measurement_noise[i][j];
-}
-
-void tick_ekf(
-    float deltaTime,
-    float gyro[3],
-    float accel[3]
-)
-{
+/* normalizes quaternion */
+void normalize(float q[4]) {
+    float norm = sqrtf(q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3]);
     
+    for (int i = 0; i < 4; i++) q[i] = q[i] / norm;
 }
 
 #define N 3
@@ -99,9 +72,80 @@ int inverse(float a[N][N], float inverse[N][N]) {
     return 1; // Success
 }
 
-/* normalizes quaternion */
-void normalize(float q[4]) {
-    float norm = sqrtf(q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3]);
-    
-    for (int i = 0; i < 4; i++) q[i] = q[i] / norm;
+/* ------------- EKF ---------------- */
+static EKF ekf;
+
+void init_ekf(
+    float process_noise[STATE_DIM][STATE_DIM],
+    float measurement_noise[3][3])
+{
+    // init
+    ekf.x[0] = 1;
+    ekf.x[1] = 0;
+    ekf.x[2] = 0;
+    ekf.x[3] = 0;
+
+    // sets ekf.P to an identity matrix
+    for (int i = 0; i < STATE_DIM; i++) for (int j = 0; j < STATE_DIM; j++) {
+        if (i==j) {
+            ekf.P[i][j] = 1;
+            continue;
+        }
+
+        ekf.P[i][j] = 0;
+    }
+
+    // copy inputs
+    for (int i = 0; i < STATE_DIM; i++) for (int j = 0; j < STATE_DIM; j++) ekf.Q[i][j] = process_noise[i][j];
+    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) ekf.R[i][j] = measurement_noise[i][j];
 }
+
+void state_transition(
+    float p[4], // previous state
+    float next_state[4],
+    float time_step,
+    float g[3] // gyro data
+)
+{
+    float P[4][1];
+    for (int i = 0; i < 4; i++) P[i][0] = p[i];
+
+    /*
+    | 0   -gx   -gy  -gz  |
+    | gx   0     gz  -gy  |
+    | gy  -gz    0    gx  |
+    | gz   gy   -gx   0   |
+    */
+
+    float B[4][4] = {{0, -g[0], -g[1], -g[2]},
+                    {g[0], 0, g[2], -g[1]},
+                    {g[1], -g[2], 0, g[0]},
+                    {g[2], g[1], -g[0], 0}};
+
+
+    float C[4][1];
+
+    MAT_MUL(B, P, C, 4, 4, 1);
+
+    for (int i = 0; i < 4; i++) C[i][0] = C[i][0] * (time_step / 2);
+
+    for (int i = 0; i < 4; i++) {
+        next_state[i] = p[i] + C[i][0];
+    }
+
+    normalize(next_state);
+}
+
+
+void tick_ekf(
+    float deltaTime,
+    float gyro[3],
+    float accel[3]
+)
+{
+    
+}
+
+
+
+
