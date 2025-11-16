8bb86712f21be31ff0c56cadcf0ea5a5f610fa28
Alex <climie.alex@gmail.com>
2025-11-15 23:52:58 -0800

    header file update
    + added get_state(). lowk dont know if my ekf.h file is right

--- Core/Inc/algorithms/ekf.h
+++ Core/Inc/algorithms/ekf.h
@@ -4,13 +4,26 @@
 #define STATE_DIM 4
 
 typedef struct {
-    float x[STATE_DIM];             // state vector (just a quaternion for now)
-    float covar[STATE_DIM][STATE_DIM];  // covariance matrix
-    float process[STATE_DIM][STATE_DIM];  // process noise
-    float measurement[3][3];                  // measurement noise (of accel)
+    float x[STATE_DIM];                    // state vector (just a quaternion for now)
+    float covar[STATE_DIM][STATE_DIM];     // covariance matrix
+    float process[STATE_DIM][STATE_DIM];   // process noise
+    float measurement[3][3];               // measurement noise (of accel)
 } EKF;
 
-/* Macro to multiply any given A (r1 x c1) by B (c1 x c2) */
+void get_state_x(float out[4]);
+
+void init_ekf(
+    float process_noise[STATE_DIM][STATE_DIM],
+    float measurement_noise[3][3]
+);
+
+void tick_ekf(
+    float deltaTime,
+    float gyro[3],
+    float accel[3]
+);
+
+/* Macro to multiply any given A (r1 x c1) by B (c1 x c2), producing (r1 x c2) */
 #define MAT_MUL(A, B, C, r1, c1, c2)                          \
     do {                                                      \
         for (int i = 0; i < (r1); ++i) {                      \
