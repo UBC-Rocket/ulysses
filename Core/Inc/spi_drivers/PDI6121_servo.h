be4f0b0067ff23d4a2ea8bea7adf1799a0cfc083
Jimmy Pan <jimmypan0410@gmail.com>
2026-01-25 11:06:32 -0800

    header setup

---
+++ Core/Inc/spi_drivers/PDI6121_servo.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "stdint.h"
+#include "stdbool.h"
+
+typedef struct {
+    void $htim;
+    uint32_t channel;
+    uint32_t timer_hz;
+    uint32_t period_ticks;
+} PDI6121_servo_pwm_t;
+
+typedef struct {
+    PDI6121_servo_pwm_t pwm;
+
+    /* Calibration parameters */
+    uint16_t us_min;
+    uint16_t us_mid;
+    uint16_t us_max;
+
+    unint16_t us_last;
+    bool enabled;
+} PDI6121_servo_t;
+
+
+void PDI6121_servo_init(PDI6121_servo_t *servo, PDI6121_servo_pwm_t *pwm);
+
+void PDI6121_servo_enable(PDI6121_servo_t *servo, bool enable);
+
+void PDI6121_servo_set_us(PDI6121_servo_t *servo, uint16_t us);
+
+void PDI6121_servo_set_norm(PDI6121_servo_t *servo, float norm);
+
+void PDI6121_servo_set_deg(PDI6121_servo_t *servo, float degrees);
+
+void PDI6121_servo_set_cal(PDI6121_servo_t *servo);
